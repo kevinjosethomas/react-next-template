ab0aa844c2cadf264b5ddd473116d3bd05e580cd
TrustedMercury <kevin.jt2007@gmail.com>
2020-12-04 22:01:03 -0500

    Updated tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+
+const colors = {
+  dark: {
+    100: "#16181C",
+    200: "#1C1D24",
+    300: "#282C35",
+    400: "#202228"
+  },
+  emph: {
+    100: "#AA85FF",
+    200: "#8C61F2"
+  }
+};
+
 module.exports = {
-  purge: ["./src/pages/**/*.js", "./src/components/**/*.js  "],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: colors
+    },
   },
   variants: {
     extend: {},
