df76029ecd1cb303904d6499828e06042bc5ae4c
TrustedMercury <kevin.jt2007@gmail.com>
2020-12-04 22:00:54 -0500

    Added content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { motion } from "framer-motion";
 
-export default function Home() {
+import { DefaultLayout } from "../layouts/DefaultLayout";
+
+const Home = (props) => {
   return (
-    <div>
-
-    </div>
+    <DefaultLayout>
+      <div className="flex flex-col items-center justify-center h-full text-center">
+        <motion.span
+          className="text-5xl md:text-8xl text-gray-300 font-semibold font-inter"
+          initial="initial"
+          animate="enter"
+          variants={{
+            initial: { y: 20, opacity: 0 },
+            enter: { y: 0, opacity: 1, transition: { duration: 0.5 } }
+          }}
+        >
+          React + Next
+        </motion.span>
+        <motion.span
+          className="text-lg md:text-3xl text-gray-400 max-w-xs md:max-w-full"
+          initial="initial"
+          animate="enter"
+          variants={{
+            initial: { y: 20, opacity: 0 },
+            enter: { y: 0, opacity: 1, transition: { duration: 0.5, delay: 0.1 } }
+          }}
+        >
+          with TailwindCSS, Framer Motion and other cool stuff
+        </motion.span>
+      </div>
+    </DefaultLayout>
   )
 }
+
+export default Home;
